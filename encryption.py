06d0d8b5dc0337421fe85dbbc2667dc61e5a70a4
Prathamesh <prathameshks2003@gmail.com>
2022-01-26 16:36:02 +0530

    making function file and adding init

---
+++ encryption.py
@@ -0,0 +1,51 @@
+from cryptography.fernet import Fernet
+import os
+
+class Encrypter:
+    def __init__(self) -> None:
+        pass
+    def encrypt(self,key,data):
+        manager = Fernet(key) 
+        e_data = manager.encrypt(data)
+        return e_data
+    
+    def decrypt(self,key,data):
+        manager = Fernet(key) 
+        d_data = manager.decrypt(data)
+        return d_data
+
+    def get_new_key(self):
+        if os.path.exists(r'files/key.txt'):
+            newkey = Fernet.generate_key()
+            with  open(r"files/key.txt","r") as file1:
+                filedata = file1.read()
+            with open(r"files/oldkey.txt",'w') as oldfile:
+                oldfile.write(filedata) 
+            with  open(r"files/key.txt","w") as file1:
+                filedata = file1.write(newkey.decode('ascii'))
+            return newkey.decode('ascii')
+        else:
+            key = Fernet.generate_key()
+            with open(r"files/key.txt","w") as file:
+                file.write(key.decode('ascii'))
+            return key.decode('ascii')
+
+            
+    def get_old_key(self):
+        if os.path.exists(r'files/oldkey.txt'):
+            with open(r"files/oldkey.txt",'r') as oldfile:
+                old_data = oldfile.read()
+                old_key = old_data.decode('ascii')
+            return old_key
+        else:
+            return False
+            
+    
+    def get_current_key(self):
+        if os.path.exists(r'files/key.txt'):
+            with open(r"files/key.txt",'r') as file:
+                key = file.read()
+            return key
+        else:
+            return self.get_new_key()
+
