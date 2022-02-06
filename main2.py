b958bfbeb715e5f39fded263579555e6b84c77e1
Prathamesh <prathameshks2003@gmail.com>
2022-02-06 22:48:45 +0530

    added reset window

--- main2.py
+++ main2.py
@@ -3,9 +3,8 @@ import json
 import getpass
 from encrypter import Encrypter
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Button, ttk
 from passlib.context import CryptContext
-from ttkthemes import ThemedStyle
 
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"],default="pbkdf2_sha256",pbkdf2_sha256__default_rounds=30000)
 def encryptpass(pw):
@@ -76,11 +75,25 @@ def click(*args):
     if first or pwentry.get()=='Enter Main Password':
         first = False
         pwentry.delete(0, 'end')
+        pwentry.config(show='*')
 
 def leave(*args):
     if pwentry.get()=='':
         pwentry.insert(0, 'Enter Main Password')
+        pwentry.config(show=None)
 
+# def showhide():
+#     val = pwshohidebtn.cget('text')
+#     print(val)
+#     if val == 'Hide':
+#         pwshohidebtn.config(text='Show')
+#         pwentry.config(show='*')
+#     else:
+#         pwshohidebtn.config(text='Hide')
+#         pwentry.config(show=None)
+#         pwentry.insert(0,pwentry.get())
+#         print("hidden")
+        
 def authenticate():
     menu = tk.Frame(win)
     menu.tkraise()
@@ -91,9 +104,44 @@ def authenticate():
     return None
 
 
-def darkstyle(root):
-    ''' Return a dark style to the window'''
+
+
+def resetpass():
+    def reset():
+        pw1 = pwentry1.get()
+        pw2 = pwentry2.get()
+        if pw1!=pw2:
+            labinfo = ttk.Label(resetpass,text='Password Missmatch',justify='center',background='red')
+            labinfo.grid(row=3,column=0,columnspan=2,sticky="nsew")
+            win.after(3500,lambda: (labinfo.destroy()))
+    resetpass = tk.Frame(win)
+    resetpass.grid(row=0, column=0, sticky='NSEW')
+        
+    resetpass.columnconfigure(0,weight=2)
+    resetpass.rowconfigure(0,weight=2)
+    resetpass.rowconfigure(1,weight=1)
+    resetpass.rowconfigure(2,weight=1)
+    # resetpass.rowconfigure(3,weight=1)
+    resetpass.rowconfigure(4,weight=2)
+    mainlab = ttk.Label(resetpass,text="Reset Password",justify='center',font=("Arial", 15))
+    mainlab.grid(row=0,column=0,columnspan=2)
+
+    pwlab1 = ttk.Label(resetpass,text='Enter New Password')
+    pwlab1.grid(row=1,column=0,sticky="esn")
+    pwentry1 = ttk.Entry(resetpass,show='*')
+    pwentry1.grid(row=1,column=1,sticky="EW",padx=30)
     
+    pwlab2 = ttk.Label(resetpass,text='Confirm   Password')
+    pwlab2.grid(row=2,column=0,sticky="esn")
+    pwentry2 = ttk.Entry(resetpass,show='*')
+    pwentry2.grid(row=2,column=1,sticky="EW",padx=30)
+    
+    resetbtn = ttk.Button(resetpass,text="Reset",command=reset)
+    resetbtn.grid(row=4,column=0,columnspan=2)
+
+    
+
+def darkstyle(root):
     style = ttk.Style(root)
     root.tk.call('source', 'files/theme/azure dark/azure dark.tcl')
     style.theme_use('azure')
@@ -103,26 +151,29 @@ def darkstyle(root):
 style = darkstyle(win)
 
 verification = tk.Frame(win)
-# style = ThemedStyle(verification)
-# style.set_theme("equilux")
 verification.grid(row=0, column=0, sticky='NSEW')
 
 verification.columnconfigure(0,weight=2)
-# verification.columnconfigure(1,weight=1)
 verification.rowconfigure(0,weight=2)
 verification.rowconfigure(1,weight=1)
 verification.rowconfigure(2,weight=2)
+verification.rowconfigure(3,weight=2)
 mainlab = ttk.Label(verification,text="Welcome To Password Wallet",justify='center',font=("Arial", 15))
 mainlab.grid(row=0,column=0)
 
-pwentry = ttk.Entry(verification)
+pwentry = ttk.Entry(verification,show=None)
 pwentry.insert(0, 'Enter Main Password')
 pwentry.grid(row=1,column=0,sticky="EW",padx=30)
+# pwshohidebtn = ttk.Button(verification,text="Show",command=showhide)
+# pwshohidebtn.grid(row=1,column=1,sticky="W")
 pwentry.bind("<Button-1>", click)
 pwentry.bind("<Leave>", leave)
 
-veribtn = ttk.Button(verification,text="Authenticate",command=authenticate)
+veribtn = ttk.Button(verification,text="         Authenticate         ",command=authenticate)
 veribtn.grid(row=2,column=0)
+
+rsbtn = ttk.Button(verification,text="Reset/Create Password",command=resetpass)
+rsbtn.grid(row=3,column=0,sticky='n')
 
 raise_frame(verification)
 
