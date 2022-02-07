c5cd8a90c3d3654abc123b165ee41e13ff776b20
Prathamesh <prathameshks2003@gmail.com>
2022-02-07 21:04:34 +0530

    basic authentication sucessfully created

--- main2.py
+++ main2.py
@@ -3,15 +3,27 @@ import json
 import getpass
 from encrypter import Encrypter
 import tkinter as tk
-from tkinter import Button, ttk
+from tkinter import messagebox, ttk
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"],default="pbkdf2_sha256",pbkdf2_sha256__default_rounds=30000)
 def encryptpass(pw):
     return pwd_context.hash(pw)
 
-def verifypass(pw,hashcode):
+def verifypass(pw):
+    hashcode = gethash()
     return pwd_context.verify(pw,hashcode)
+    
+
+def sethash(pw):
+    with open('files/hash.txt','w') as hashfile:
+        hashfile.write(encryptpass(pw))
+        return True
+def gethash():
+    with open('files/hash.txt','r') as hashfile:
+        hash = hashfile.read()
+        return hash
+
 
 win = tk.Tk()
 win.geometry('300x300')
@@ -55,32 +67,9 @@ def get_current_data():
         print("got here")
         return {}
 
-
-def updatemainpass(dictraw):
-    password = getpass.getpass("Enter Profile Password For setting First Time: ")
+def updatemainpass(dictraw,password):
     dictraw['MPW'] = {'PW': password}
     return dictraw
-
-
-def startfirst():
-    if get_current_data() == {}:
-        store_data(updatemainpass({}))
-
-
-startfirst()
-
-first = True
-def click(*args):
-    global first
-    if first or pwentry.get()=='Enter Main Password':
-        first = False
-        pwentry.delete(0, 'end')
-        pwentry.config(show='*')
-
-def leave(*args):
-    if pwentry.get()=='':
-        pwentry.insert(0, 'Enter Main Password')
-        pwentry.config(show=None)
 
 # def showhide():
 #     val = pwshohidebtn.cget('text')
@@ -93,51 +82,106 @@ def leave(*args):
 #         pwentry.config(show=None)
 #         pwentry.insert(0,pwentry.get())
 #         print("hidden")
-        
-def authenticate():
-    menu = tk.Frame(win)
-    menu.tkraise()
-    menu.grid(row=0,column=0,sticky="NSEW")
-        
-    menu_op1 = tk.Button(menu,text="View Saved")
-    menu_op1.grid(row=0,column=0)
-    return None
+
+def startmainapp():
+    main = tk.Frame(win)
+    main.grid(row=0,column=0,sticky="NSEW")
+    main.tkraise()
+    ttk.Label(main,text="Auth Sucess").pack()
+
+def authenticate(pw):
+    if verifypass(pw):
+        startmainapp()
+    else:
+        oldstart("Wrong Password",color='red')
+
+
+def newstart():
+    def set():
+        global dictraw
+        pw1 = pwentry1.get()
+        pw2 = pwentry2.get()
+        if pw1!=pw2:
+            labinfo = ttk.Label(setpass,text='Password Missmatch',justify='center',background='red')
+            labinfo.grid(row=3,column=0,columnspan=2,sticky="nsew")
+            win.after(3500,lambda: (labinfo.destroy()))
+        else:
+            if messagebox.askokcancel('Confermation',"Do you really want to set password \nThis operation is ireversiable."):
+                sethash(pw1)   
+                oldstart("Password created Sucessfully")          
+            else:
+                newstart()
+    setpass = tk.Frame(win)
+    setpass.grid(row=0, column=0, sticky='NSEW')
+    setpass.tkraise()
+
+    setpass.columnconfigure(0,weight=2)
+    setpass.rowconfigure(0,weight=2)
+    setpass.rowconfigure(1,weight=1)
+    setpass.rowconfigure(2,weight=1)
+    # setpass.rowconfigure(3,weight=1)
+    setpass.rowconfigure(4,weight=2)
+    mainlab = ttk.Label(setpass,text="Create New Password",justify='center',font=("Arial", 15))
+    mainlab.grid(row=0,column=0,columnspan=2)
+
+    pwlab1 = ttk.Label(setpass,text='Enter New Password')
+    pwlab1.grid(row=1,column=0,sticky="esn")
+    pwentry1 = ttk.Entry(setpass,show='*')
+    pwentry1.grid(row=1,column=1,sticky="EW",padx=30)
+    
+    pwlab2 = ttk.Label(setpass,text='Confirm   Password')
+    pwlab2.grid(row=2,column=0,sticky="esn")
+    pwentry2 = ttk.Entry(setpass,show='*')
+    pwentry2.grid(row=2,column=1,sticky="EW",padx=30)
+    
+    setbtn = ttk.Button(setpass,text="set",command=set)
+    setbtn.grid(row=4,column=0,columnspan=2)
+
+    
 
 
 
 
 def resetpass():
     def reset():
+        global dictraw
         pw1 = pwentry1.get()
         pw2 = pwentry2.get()
         if pw1!=pw2:
             labinfo = ttk.Label(resetpass,text='Password Missmatch',justify='center',background='red')
-            labinfo.grid(row=3,column=0,columnspan=2,sticky="nsew")
+            labinfo.grid(row=4,column=0,columnspan=2,sticky="nsew")
             win.after(3500,lambda: (labinfo.destroy()))
+        else:
+            if messagebox.askokcancel('Confermation',"Do you really want to reset password \nThis operation is ireversiable."):
+                sethash(pw1)
+                oldstart("Password Reseted Sucessfully")
+            else:
+                resetpass()
     resetpass = tk.Frame(win)
     resetpass.grid(row=0, column=0, sticky='NSEW')
-        
+    resetpass.tkraise()
     resetpass.columnconfigure(0,weight=2)
-    resetpass.rowconfigure(0,weight=2)
-    resetpass.rowconfigure(1,weight=1)
+    resetpass.rowconfigure(1,weight=2)
     resetpass.rowconfigure(2,weight=1)
-    # resetpass.rowconfigure(3,weight=1)
-    resetpass.rowconfigure(4,weight=2)
+    resetpass.rowconfigure(3,weight=1)
+    # resetpass.rowconfigure(4,weight=1)
+    resetpass.rowconfigure(5,weight=2)
     mainlab = ttk.Label(resetpass,text="Reset Password",justify='center',font=("Arial", 15))
-    mainlab.grid(row=0,column=0,columnspan=2)
-
+    mainlab.grid(row=1,column=0,columnspan=2)
+    btnback = ttk.Button(resetpass,text="Back",command=oldstart)
+    btnback.grid(row=0,column=1,sticky='e',columnspan=1)
     pwlab1 = ttk.Label(resetpass,text='Enter New Password')
-    pwlab1.grid(row=1,column=0,sticky="esn")
+    pwlab1.grid(row=2,column=0,sticky="esn")
     pwentry1 = ttk.Entry(resetpass,show='*')
-    pwentry1.grid(row=1,column=1,sticky="EW",padx=30)
+    pwentry1.grid(row=2,column=1,sticky="EW",padx=30)
     
     pwlab2 = ttk.Label(resetpass,text='Confirm   Password')
-    pwlab2.grid(row=2,column=0,sticky="esn")
+    pwlab2.grid(row=3,column=0,sticky="esn")
     pwentry2 = ttk.Entry(resetpass,show='*')
-    pwentry2.grid(row=2,column=1,sticky="EW",padx=30)
+    pwentry2.grid(row=3,column=1,sticky="EW",padx=30)
     
     resetbtn = ttk.Button(resetpass,text="Reset",command=reset)
-    resetbtn.grid(row=4,column=0,columnspan=2)
+    resetbtn.grid(row=5,column=0,columnspan=2)
 
     
 
@@ -150,35 +194,55 @@ def darkstyle(root):
     return style
 style = darkstyle(win)
 
-verification = tk.Frame(win)
-verification.grid(row=0, column=0, sticky='NSEW')
+def oldstart(*args,color='green'):
+    if os.path.exists('files/hash.txt'):
+        first = True
+        def click(*args,first=first):
+            if first or pwentry.get()=='Enter Main Password':
+                first = False
+                pwentry.delete(0, 'end')
+                pwentry.config(show='*')
 
-verification.columnconfigure(0,weight=2)
-verification.rowconfigure(0,weight=2)
-verification.rowconfigure(1,weight=1)
-verification.rowconfigure(2,weight=2)
-verification.rowconfigure(3,weight=2)
-mainlab = ttk.Label(verification,text="Welcome To Password Wallet",justify='center',font=("Arial", 15))
-mainlab.grid(row=0,column=0)
+        def leave(*args):
+            if pwentry.get()=='':
+                pwentry.insert(0, 'Enter Main Password')
+                pwentry.config(show=None)
 
-pwentry = ttk.Entry(verification,show=None)
-pwentry.insert(0, 'Enter Main Password')
-pwentry.grid(row=1,column=0,sticky="EW",padx=30)
-# pwshohidebtn = ttk.Button(verification,text="Show",command=showhide)
-# pwshohidebtn.grid(row=1,column=1,sticky="W")
-pwentry.bind("<Button-1>", click)
-pwentry.bind("<Leave>", leave)
+        verification = tk.Frame(win)
+        verification.grid(row=0, column=0, sticky='NSEW')
 
-veribtn = ttk.Button(verification,text="         Authenticate         ",command=authenticate)
-veribtn.grid(row=2,column=0)
+        verification.columnconfigure(0,weight=2)
+        verification.rowconfigure(0,weight=2)
+        verification.rowconfigure(1,weight=1)
+        verification.rowconfigure(2,weight=2)
+        verification.rowconfigure(3,weight=2)
+        mainlab = ttk.Label(verification,text="Welcome To Password Wallet",justify='center',font=("Arial", 15))
+        mainlab.grid(row=0,column=0)
 
-rsbtn = ttk.Button(verification,text="Reset/Create Password",command=resetpass)
-rsbtn.grid(row=3,column=0,sticky='n')
+        pwentry = ttk.Entry(verification,show=None)
+        pwentry.insert(0, 'Enter Main Password')
+        pwentry.grid(row=1,column=0,sticky="EW",padx=30)
+        # pwshohidebtn = ttk.Button(verification,text="Show",command=showhide)
+        # pwshohidebtn.grid(row=1,column=1,sticky="W")
+        pwentry.bind("<Button-1>", click)
+        pwentry.bind("<Leave>", leave)
 
-raise_frame(verification)
+        veribtn = ttk.Button(verification,text="         Authenticate         ",command=lambda:(authenticate(pwentry.get())))
+        veribtn.grid(row=2,column=0)
 
+        rsbtn = ttk.Button(verification,text="Reset/Create Password",command=resetpass)
+        rsbtn.grid(row=3,column=0,sticky='n')
 
+        messageonveri = ttk.Label(verification,justify='center',foreground=color,font=('arial',10))
+        messageonveri.grid(row=4,column=0,sticky='n',pady=10)
+        if len(args) == 1:
+            messageonveri.config(text=args[0])
+            win.after(3500,lambda: (messageonveri.destroy()))
+        raise_frame(verification)
+    else:
+        newstart()
 
+oldstart()
 
 
 win.mainloop()
